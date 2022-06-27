e0ef84a271c8acea84d26c7abc28abd3d7e8a010
YunyiShen <yshen99@wisc.edu>
2022-06-27 15:21:59 -0400

    run on win server

--- repeat_Chapron_2016/run_models.R
+++ repeat_Chapron_2016/run_models.R
@@ -1,14 +1,32 @@
 load("./data/chapron_treves_2016.Rdata")
 library(rstan)
+library(bridgesampling)
+
+exp_null <- stan_model("./stan//exp_null.stan", 
+                       model_name = "exp_null")
+
+exp_policy <- stan_model("./stan//exp_policy.stan", 
+                         model_name = "exp_policy")
+
+logistic_null <- stan_model("./stan//logistic_null.stan", 
+                            model_name = "logistic_null")
+
+logistic_policy <- stan_model("./stan//logistic_policy.stan", 
+                            model_name = "logistic_policy")
+
 
 options(mc.cores = parallel::detectCores())
 rstan_options(auto_write = TRUE)
+
+set.seed(42)
 
 WI_data <- list(N_year = 18,
                 min_count = wisconsin$Nobs.Min,
                 max_count = wisconsin$Nobs.Max,
                 H_t = wisconsin$H,
-                D_t = treatment$days_cull_WI)
+                D_t = treatment$days_cull_WI/365)
+
+
 
 initial_value <- list(N_t = (wisconsin$Nobs.Max),
                       psi_t_min = wisconsin$Nobs.Min,
@@ -16,36 +34,69 @@ initial_value <- list(N_t = (wisconsin$Nobs.Max),
                       sigma_proc = 0.06,
                       gamma = 1.06,
                       beta_0 = 0.16,
-                      beta_1 = 0,
-                      linvK = log(max(wisconsin$Nobs.Max)),
+                      beta_1 = -.5,
+                      K = (max(wisconsin$Nobs.Max)),
                       o_min = 0.97,
                       o_max = 1.03,
-                      sigma_obs_min = 3.82,
-                      sigma_obs_max = 4.72,
-                      mu_0 = log(wisconsin$Nobs.Max[1]))
+                      sigma_obs = 3.82
+                      )
 
-exp_null <- stan_model("./stan//exp_null.stan", 
-            model_name = "exp_null")
 
+
+## Take samples
 
 sample_exp_null <- sampling(exp_null, 
                             data = WI_data,
-                            control = list(adapt_delta = 0.9995, max_treedepth=30), 
-                            iter = 15000,chains = 2,
-                            init = list(chain1 = initial_value,chain2 = initial_value),
-                            pars = c("sigma_proc","gamma","beta_0","o_min","o_max","sigma_obs_min","sigma_obs_max"), 
-                            include = T)
-
-exp_policy <- stan_model("./stan//exp_policy.stan", 
-                       model_name = "exp_policy")
+                            control = list(adapt_delta = 0.999, max_treedepth=30), 
+                            iter = 15000,chains = 4, thin = 5, 
+                            init = list(initial_value,initial_value,initial_value,initial_value),
+                            )#pars = c("sigma_proc","gamma","beta_0","o_min","o_max","sigma_obs"), 
+                            #include = T)
 
 
 sample_exp_policy <- sampling(exp_policy, 
                             data = WI_data,
-                            control = list(adapt_delta = 0.9995, max_treedepth=30), 
-                            iter = 15000,chains = 2,
-                            init = list(chain1 = initial_value, chain2 = initial_value),
-                            pars = c("sigma_proc","gamma","beta_0","beta_1","o_min","o_max","sigma_obs_min","sigma_obs_max"), 
-                            include = T)
+                            control = list(adapt_delta = 0.999, max_treedepth=30), 
+                            iter = 15000,chains = 4,thin = 5,
+                            init = list(initial_value,initial_value,initial_value,initial_value),
+                            )#pars = c("sigma_proc","gamma","beta_0","beta_1","o_min","o_max","sigma_obs"), 
+                            #include = T)
+
+
+sample_logistic_null <- sampling(logistic_null, 
+                                 data = WI_data,
+                                 control = list(adapt_delta = 0.999, max_treedepth=30), 
+                                 iter = 15000,chains = 4,thin = 5,
+                                 init = list(initial_value,initial_value,initial_value,initial_value))#,
+#pars = c("sigma_proc","gamma","beta_0","o_min","o_max","sigma_obs","K"), 
+#include = T)
+
+sample_logistic_policy <- sampling(logistic_policy, 
+                                 data = WI_data,
+                                 control = list(adapt_delta = 0.999, max_treedepth=30), 
+                                 iter = 15000,chains = 4,thin = 5,
+                                 init = list(initial_value,initial_value,initial_value,initial_value)
+)#pars = c("sigma_proc","gamma","beta_0","o_min","o_max","sigma_obs"), 
+#include = T)
+
+
+## BF for inference 
+
+normalizing_exp_policy <- bridge_sampler(sample_exp_policy)
+normalizing_exp_null <- bridge_sampler(sample_exp_null)
+
+normalizing_logistic_policy <- bridge_sampler(sample_logistic_policy)
+normalizing_logistic_null <- bridge_sampler(sample_logistic_null)
+
+bayes_factor_exp_policy <- bridgesampling::bf(normalizing_exp_policy, normalizing_exp_null)
+bayes_factor_logistic_null <- bridgesampling::bf(normalizing_logistic_null, normalizing_exp_null)
+bayes_factor_logistic_policy <- bridgesampling::bf(normalizing_logistic_policy, normalizing_exp_null)
+
+
+bayes_factor_logistic_vs_exp_policy <- bridgesampling::bf(normalizing_logistic_null, normalizing_exp_policy)
+
+save.image("./model_res.RData")
+
+
 
 
--- repeat_Chapron_2016/stan/logistic_policy.stan
+++ repeat_Chapron_2016/stan/logistic_policy.stan
@@ -3,7 +3,7 @@ data {
     int<lower = 0> min_count[N_year]; //winter minimum count
     int<lower = 0> max_count[N_year]; //winter maximum count
     int<lower = 0> H_t[N_year]; //harvest
-    int<lower = 0> D_t[N_year]; //days culled
+    real<lower = 0, upper = 1> D_t[N_year]; //days culled
 }
 
 parameters {
@@ -12,17 +12,14 @@ parameters {
     real<lower = 0> gamma;
     real beta_0;
     real beta_1; //the "policy signal", useful in non-null's 
-    real linvK; // inverse carrying capacity
+    real K; // inverse carrying capacity
     real<lower = 0, upper = 1> o_min; //detection rate for min
     real<lower = 1, upper = 10> o_max; //detection multiplier for max
-    real<lower = 0> sigma_obs_min; // observ error
-    real<lower = 0> sigma_obs_max; // observ error
-    real mu_0; //baseline year 
+    real<lower = 0> sigma_obs; // observ error
     
     // some rather local ones
     vector<lower = 0>[N_year] N_t; // true population
-    vector<lower = 0>[N_year] psi_t_min; // mean observed
-    vector<lower = 0>[N_year] psi_t_max;
+    vector<lower = 0>[N_year] psi_t; // mean observed
     
 }
 
@@ -34,42 +31,39 @@ model {
     // priors, hyper parameters from their supp
     sigma_proc ~ uniform(0 , 0.5);
     gamma ~ normal(1.06, 0.267261); // sqrt(1/14)
-    beta_0 ~ normal(0 , 1e3);
+    beta_0 ~ normal(0 , 1e6);
     beta_1 ~ normal(0 , 1e3);
-    sigma_obs_min ~ uniform(0,100);
-    sigma_obs_max ~ uniform(0,100);
+    sigma_obs ~ uniform(0,100);
 
     o_min~uniform(0,1); // these two are slightly different but should not cause problem
     o_max~uniform(1,10); 
-    
-    mu_0 ~ normal(0,1e3);
+    K ~ lognormal(8,1);
 
-    linvK ~ normal(0,1000);
+    #linvK ~ normal(0,1000);
 
     // likelihood
-    N_t[1] ~ lognormal(mu_0,sigma_proc);
+    N_t[1] ~ gamma(1e6,1e6); //lognormal(mu_0,sigma_proc);
 
     for(tt in 1:N_year){
         // this years observation 
         //print(psi_t_min);
-        target += gamma_lpdf(psi_t_min[tt] | (N_t[tt]/sigma_obs_min)^2, N_t[tt]/((sigma_obs_min)^2) );
-        target += gamma_lpdf(psi_t_max[tt] | (N_t[tt]/sigma_obs_max)^2, N_t[tt]/((sigma_obs_max)^2) );
-        target += poisson_lpmf(min_count[tt] | o_min * psi_t_min[tt]);
-        target += poisson_lpmf(max_count[tt] | o_max * psi_t_max[tt]);
+        target += gamma_lpdf(psi_t[tt] | (N_t[tt]/sigma_obs)^2, N_t[tt]/((sigma_obs)^2) );
+        target += poisson_lpmf(min_count[tt] | o_min * psi_t[tt]);
+        target += poisson_lpmf(max_count[tt] | o_max * psi_t[tt]);
         // things about grwoth 
         if(tt < N_year){
             //r_t = beta_0; // growth rate
             r_t = beta_0 + beta_1 * D_t[tt];
             //mu_t = log(N_t[tt] * exp(r_t) - gamma * H_t[tt+1]); // exponential grwoth
             
-            mu_t = log(N_t[tt] * exp(r_t) * (1-exp(linvK) * N_t[tt]) - gamma * H_t[tt]); // logistic
+            mu_t = log( N_t[tt] * (1 + (exp(r_t)-1) * (1-N_t[tt]/K) )- gamma * H_t[tt+1]);
+            
             N_t[tt+1] ~ lognormal(mu_t, sigma_proc);
         }
 
     }
 
 }
-
 
 
 
