c9dda2e7aa4ac9d949e8703499bd9c97d0ff5c7b
YunyiShen <yshen99@wisc.edu>
2022-07-22 12:12:21 -0400

    minor change to the figs

--- new_Analysis/by_zone.R
+++ new_Analysis/by_zone.R
@@ -1,5 +1,5 @@
 wi_zone <- read.csv("../data/WI_zone_wolfnumbers_07202022.csv")
-focused_zone <- c("1","2","5")
+focused_zone <- c("1","2","3","4","5","6","x")
 
 for(zone in focused_zone){
   temp <- wi_zone[wi_zone$Zone==zone, ]
@@ -21,7 +21,8 @@ for(zone in focused_zone){
   points(temp$Y[c(34:41)]
          ,temp$Wolves[c(34:41)]
          ,pch = 7)
-  
+  abline(v = 1987+(-sum(log(logistic_fit$m$getPars()[c("p0","invK")])))/logistic_fit$m$getPars()[c("r")], lty = 2)
+  abline(h = .5/logistic_fit$m$getPars()["invK"], lty = 2)
   #legend("topleft",legend = c("observed:pre-hunting","observed:post-hunting",
                               
   #                            "logistic"), 
@@ -34,25 +35,25 @@ area_pop_lm <- lm(Population~Range-1, area_pop)
 area_pop_pred <- predict(area_pop_lm, se = T)
 
 
-png("./figs/pop_vs_range_by_zone.png", width = 6, height = 3.5, res = 500, unit = "in")
+png("./figs/pop_vs_range_by_zone_all.png", width = 6, height = 3.5, res = 500, unit = "in")
 
 par(mar = c(3,3,2,2), mgp = c(1.8, 0.5, 0))
 
 plot(wolf_range$Winter.Minimum.Count, area_pop_pred$fit, xlab = "Range", ylab = "Population", type = "l", xlim = c(0,15000), ylim = c(0,450))
 abline(area_pop_lm)
 
-for(zone in focused_zone){
+focused_zone2 <- c("1","2","3","4","5","6")
+for(zone in focused_zone2){
   temp <- wi_zone[wi_zone$Zone==zone, ]
   #png(paste0("./figs/population_zone_",zone,".png"), width = 4, height = 3, res = 500, unit = "in")
   points(temp$Pack.Area.in.Zone.m2/1e6,temp$Wolves, pch = as.numeric(zone))
   
 }
 
-legend("topleft",legend = c("zone 1","zone 2",
-                            "zone 5",
-
+legend("topleft",legend = c(paste("zone",focused_zone2),
                             "overall fit"), 
-       lty = c(NA,NA,NA,1), pch = c(1,2,5,NA))
+       lty = c(rep(NA,length(focused_zone2)),1), pch = c(as.numeric(focused_zone2),NA),
+       )
 dev.off()
 
 
